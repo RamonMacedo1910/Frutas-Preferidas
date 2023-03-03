1d05c89a7a293f41b3bf775a13028c7601631faf
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-03 20:37:22 -0300

    Finaliza exercício Frutas-Preferidas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Minhas frutas preferidas ---");
+Console.WriteLine();
+Console.WriteLine("1. Laranja");
+Console.WriteLine("2. Melancia");
+Console.WriteLine("3. Uva");
+Console.WriteLine("4. Manga");
+Console.WriteLine("5. Pêssego");
